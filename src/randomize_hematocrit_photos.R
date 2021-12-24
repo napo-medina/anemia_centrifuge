7258d28820139747e674876f81a8a21c1474cd12
Daniel Medina-Neira <danielmedinaneira555@gmail.com>
2021-12-24 10:13:41 -0500

    Randomize photos and then decode the randomization to obtain hematocrit from photos blindly

---
+++ src/randomize_hematocrit_photos.R
@@ -0,0 +1,61 @@
+
+library(tibble)
+library(tools)
+library(dplyr)
+library(readr)
+library(xlsx)
+library(here)
+library(purrr)
+
+
+# Randomize photos ------------
+
+# Create master data frame
+files <- list.files(here("data/hematocrit_photos/original"))
+create_master_df <- function(files) {
+  create_new_names <- function(x) {
+    extensions <- file_ext(x)
+    new_names <- formatC(seq_along(x), width = 2, format = "d", flag = "0")
+    new_names <- paste0(new_names, ".", extensions)
+    new_names
+  }
+  tibble(
+    id = seq_along(files),
+    original = sample(files),
+    new = create_new_names(original)
+  )
+}
+set.seed(123)
+master_df <- create_master_df(files)
+
+# Copy images
+file.copy(
+  from = here("data/hematocrit_photos/original", master_df$original),
+  to = here("data/hematocrit_photos/random", master_df$new)
+)
+
+# Create blind data frame
+blind_df <- select(master_df, -original)
+blind_df <- mutate(blind_df, hematocrit = "")
+
+# Write data frames
+write_csv(master_df, here("data/hematocrit_photos/master_df.csv"))
+write.xlsx(blind_df, here("data/hematocrit_photos/blind_df.xlsx"))
+
+
+# Reveal / decode photos -------------------
+
+master_df <- read_csv(here("data/hematocrit_photos/master_df.csv"))
+hematocrit_df <- tibble(
+  read.xlsx(here("data/hematocrit_photos/blind_df.xlsx"), sheetIndex = 1)
+)
+join_df <- function(master_df, hematocrit_df, author) {
+  res <- left_join(master_df, hematocrit_df, by = c("id", "new"))
+  res <- rename(res, "hematocrit_{author}" := hematocrit)
+  res
+}
+
+join_df(master_df, hematocrit_df, "daniel")
+
+# reduce2(list(), authors, join_df)
+
