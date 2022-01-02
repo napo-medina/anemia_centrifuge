075887b6ff656be45b76c9e675a7071c4a9f44f3
Daniel Medina-Neira <danielmedinaneira555@gmail.com>
2022-01-02 09:53:25 -0500

    Randomize the IDs to create the final database

--- data-raw/05_deidentify_data.R
+++ data-raw/05_deidentify_data.R
@@ -3,11 +3,13 @@ library(here)
 library(dplyr)
 
 dat <- read_csv(here("data-raw/database_complete.csv"))
-dat <- dat %>% select(-id)
-i <- sample(nrow(dat))
-dat <- dat[i, ]
 
-dat <- dat %>% mutate(across(c(sexo, operario, dispositivo), as.factor))
+# Randomize ID
+i <- unique(dat$id)
+indx_vector <- setNames(sample(i), i)
+dat <- dat %>% mutate(id = indx_vector[id]) %>% arrange(id)
+
+dat <- dat %>% mutate(across(c(sex, operator, device, method), as.factor))
 
 write_csv(dat, here("data/database.csv"))
 write_rds(dat, here("data/database.rds"))
