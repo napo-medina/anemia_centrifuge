b2e0516a4c2c8caf8ac0d31d85a38c702eeccbad
Daniel Medina-Neira <danielmedinaneira555@gmail.com>
2022-01-03 17:57:54 -0500

    Create exploratory graphs

--- src/analysis.R
+++ src/analysis.R
@@ -6,11 +6,25 @@ library(tidyr)
 
 dat <- read_rds(here("data/database.rds"))
 
-# Exploratory analysis -----------
+# Exploratory analysis -------------
 
+# Differences between the hematocrit values obtained by centrifuge or runrun
 dat_plot <- dat %>% pivot_wider(names_from = method, values_from = hematocrit)
 
-ggplot(dat_plot) +
+base_diff_plot <- ggplot(dat_plot) +
   geom_point(aes(id, runrun), color = "blue") +
   geom_point(aes(id, centrifuge), color = "red") +
   geom_linerange(aes(id, ymax = runrun, ymin = centrifuge))
+
+base_diff_plot +
+  facet_wrap(vars(operator))
+
+base_diff_plot +
+  facet_wrap(vars(device))
+  
+
+dat_plot <- dat_plot %>% mutate(diff = runrun - centrifuge)
+ggplot(dat_plot) +
+  geom_col(aes(id, diff)) +
+  facet_wrap(vars(as.factor(time_centrifuge)))
+
