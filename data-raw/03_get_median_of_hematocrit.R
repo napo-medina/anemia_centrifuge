230ccfdc8bbd94b61d984731e872094bf17f5fd0
Daniel Medina-Neira <danielmedinaneira555@gmail.com>
2022-01-09 18:59:33 -0500

    Filter hematocrit values that were only determined by one person

--- data-raw/03_get_median_of_hematocrit.R
+++ data-raw/03_get_median_of_hematocrit.R
@@ -11,7 +11,12 @@ dat <- dat %>% select(id, original, starts_with("hematocrit"))
 res <- dat %>%
   pivot_longer(starts_with("hematocrit")) %>%
   group_by(id, original) %>%
-  summarize(hematocrit = median(value, na.rm = TRUE))
+  summarize(
+    hematocrit = median(value, na.rm = TRUE),
+    count_na = sum(is.na(value))
+  ) %>%
+  filter(count_na <= 2) %>%  # Remove values that were only determined by one person
+  select(-count_na)
 
 # Write
 write_csv(res, here("data-raw/hematocrit_values.csv"))
